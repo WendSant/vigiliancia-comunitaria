7bfdccaea60dfdb983ebe6403dbcbb2d7348258a
Wendson Santana <wendsonsantanajps@gmail.com>
2024-01-20 20:22:14 -0300

    added: ao clicar na tela abre modal

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart'
     show FlutterMap, MapOptions, Marker, MarkerLayer, TileLayer;
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart' as loc;
 import 'package:flutter/foundation.dart'; // for FutureBuilder
@@ -14,6 +16,8 @@ class _LeafletMapWidgetState extends State<LeafletMapWidget> {
   loc.Location _location = loc.Location();
   LatLng? _currentPosition;
   Future<void>? _userLocationFuture;
+  LatLng? _selectedPosition;
+  Marker? _tempMarker;
 
   @override
   void initState() {
@@ -47,42 +51,75 @@ class _LeafletMapWidgetState extends State<LeafletMapWidget> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _userLocationFuture,
-      builder: (context, snapshot) {
-        print(snapshot.connectionState);
-        if (snapshot.connectionState == ConnectionState.done) {
-          return FlutterMap(
-            options: MapOptions(
-              initialCenter: _currentPosition ?? LatLng(0.0, 0.0),
-              initialZoom: 12,
-              onTap: (tapPosition, point) => {
-                print(point.toString()),
-              },
-            ),
+  void _showModal(LatLng point) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: _currentPosition ?? LatLng(0.0, 0.0),
-                    child:
-                        Icon(Icons.location_on, size: 35, color: Colors.blue),
-                  ),
-                ],
-              ),
+              Text('Latitude: ${point.latitude}'),
+              Text('Longitude: ${point.longitude}'),
+              // Adicione mais campos ou widgets aqui
             ],
-          );
-        } else {
-          return Center(
-              child: CircularProgressIndicator()); // Loading indicator
-        }
+          ),
+        );
       },
+    ).then((_) {
+      // Quando o modal é fechado, remova o marcador temporário
+      setState(() {
+        _tempMarker = null;
+      });
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _userLocationFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return FlutterMap(
+              options: MapOptions(
+                initialCenter: _currentPosition ?? LatLng(0.0, 0.0),
+                initialZoom: 12,
+                onTap: (tapPosition, point) {
+                  setState(() {
+                    _selectedPosition = point;
+                    _tempMarker = Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: point,
+                      child: Icon(Icons.location_on),
+                    );
+                  });
+                  _showModal(point);
+                },
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+                if (_tempMarker != null) MarkerLayer(markers: [_tempMarker!]),
+                CurrentLocationLayer(
+                  followOnLocationUpdate: FollowOnLocationUpdate.always,
+                  positionStream: LocationMarkerDataStreamFactory()
+                      .fromGeolocatorPositionStream(
+                    stream: Geolocator.getPositionStream(),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            ); // Loading indicator
+          }
+        },
+      ),
     );
   }
 }
